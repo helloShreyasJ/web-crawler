829b05da960c3a964594ec97f8c6d23db5c4adbc
Shreyas <helloshreyasj@gmail.com>
2025-04-25 13:19:59 +0100

    Created function normaliseURl. Added testcases for normaliseURL.

---
+++ crawl.test.js
@@ -0,0 +1,26 @@
+const { normaliseURL } = require('./crawl.js');
+const { test, expect } = require('@jest/globals')
+
+test('To see if normaliseURL strips the protocol (HTTPS)', () => {
+  const input = 'https://blog.boot.dev/path';
+  const actual = normaliseURL(input);
+  const expected = 'blog.boot.dev/path';
+
+  expect(actual).toEqual(expected);
+})
+
+test('To see if normaliseURL strips the protocol (HTTP)', () => {
+  const input = 'http://blog.boot.dev/path';
+  const actual = normaliseURL(input);
+  const expected = 'blog.boot.dev/path';
+
+  expect(actual).toEqual(expected);
+})
+
+test('To see if normaliseURL strips trailing slash', () => {
+  const input = 'https://blog.boot.dev/path/';
+  const actual = normaliseURL(input);
+  const expected = 'blog.boot.dev/path';
+
+  expect(actual).toEqual(expected);
+})
