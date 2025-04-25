2faf25d49ee76d2524e23d8427b7bd3f30beda49
Shreyas <helloshreyasj@gmail.com>
2025-04-25 13:28:20 +0100

    Added a test case

--- crawl.test.js
+++ crawl.test.js
@@ -24,3 +24,11 @@ test('To see if normaliseURL strips trailing slash', () => {
 
   expect(actual).toEqual(expected);
 })
+
+test('To see if normaliseURL ignores case', () => {
+  const input = 'https://BloG.Boot.Dev/path/';
+  const actual = normaliseURL(input);
+  const expected = 'blog.boot.dev/path';
+
+  expect(actual).toEqual(expected);
+})
