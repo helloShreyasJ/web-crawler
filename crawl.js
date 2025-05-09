9194ea78abfcd0812ba09c06dd29341c00df06ad
Shreyas <helloshreyasj@gmail.com>
2025-05-09 12:31:14 +0100

    getURLsFromHTML and tests

--- crawl.js
+++ crawl.js
@@ -1,10 +1,37 @@
-function normaliseURL(urlString) {
-  const urlObj = new URL(urlString);
-  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
-  if(hostPath.length > 0 && hostPath.slice(-1) === '/') {
-    return hostPath.slice(0, -1);
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll('a');
+  for(const linkElement of linkElements) {
+    if(linkElement.href.slice(0, 1) === "/") { //If the href begins with / then this is an absolute url. Eg: (/path/)
+      try{
+        const URLObject = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(`${URLObject.href}`);
+      } catch(err) {
+        console.log(`Invalid Relative URL entered, error message: ${err.message}`);
+      }
+      //relative
+    } else {
+      try{
+        const URLObject = new URL(linkElement.href);
+        urls.push(URLObject.href);
+      } catch(err) {
+        console.log(`Invalid Absolute URL entered, error message: ${err.message}`);
+      }
+    }
   }
-  return hostPath;
+  return urls;
 }
 
-module.exports = { normaliseURL };
+function normaliseURL(URLString) {
+  const urlObject = new URL(URLString);
+  const hostPath = `${urlObject.hostname}${urlObject.pathname}`;
+  if(hostPath.length > 0 && hostPath.slice(-1) === '/') {
+    return hostPath.slice(0, -1); //Returns everything BUT the last character
+  } 
+  return hostPath;
+}
+module.exports = { normaliseURL, getURLsFromHTML};
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normaliseURL } = require('./crawl.js');
+const { normaliseURL, getURLsFromHTML } = require('./crawl.js');
 const { test, expect } = require('@jest/globals')
 
 test('To see if normaliseURL strips the protocol (HTTPS)', () => {
@@ -30,5 +30,62 @@ test('To see if normaliseURL ignores case', () => {
   const actual = normaliseURL(input);
   const expected = 'blog.boot.dev/path';
 
+  expect(actual).toEqual(expected);
+})
+
+test('To see if getURLsFromHTML can retrieve the URLs from HTML (absolute)', () => {
+  const input = `
+    <html>
+      <body>
+        <a href="https://blog.boot.dev">Boot.dev blog</a>
+      </body>
+    </html>
+  `;
+  const inputBaseURL = 'https://blog.boot.dev';
+  const actual = getURLsFromHTML(input, inputBaseURL);
+  const expected = ["https://blog.boot.dev/"];
+  expect(actual).toEqual(expected);
+})
+
+test('To see if getURLsFromHTML can retrieve the URLs from the HTML (relative)', () => {
+  const inputHTML = `
+    <html>
+      <body>
+        <a href="/path/">Blog boot.dev</a>
+      </body>
+    </html>
+  `
+  const inputBaseURL = 'https://blog.boot.dev';
+  const actual = getURLsFromHTML(inputHTML,inputBaseURL);
+  const expected = ["https://blog.boot.dev/path/"];
+  expect(actual).toEqual(expected);
+})
+
+test('To see if getURLsFromHTML can retrieve multiple URLS from the HTML (both relative and absolute)', () => {
+  const inputHTML = `
+    <html>
+      <body>
+        <a href="https://blog.boot.dev/path1/">Blog boot.dev path 1</a>
+        <a href="/path2/">Blog boot.dev path 2</a>
+      </body>
+    </html>
+  `
+  const inputBaseURL = 'https://blog.boot.dev';
+  const actual = getURLsFromHTML(inputHTML,inputBaseURL);
+  const expected = ["https://blog.boot.dev/path1/","https://blog.boot.dev/path2/"];
+  expect(actual).toEqual(expected);
+})
+test('To see if getURLsFromHTML can evade invalid URLs whilst returning an array of valid URLs', () => {
+  const inputHTML = `
+    <html>
+      <body>
+        <a href="invalid">Invalid URL</a>
+        <a href="/path/">Blog boot.dev path 2</a>
+      </body>
+    </html>
+  `
+  const inputBaseURL = 'https://blog.boot.dev';
+  const actual = getURLsFromHTML(inputHTML,inputBaseURL);
+  const expected = ["https://blog.boot.dev/path/"];
   expect(actual).toEqual(expected);
 })
